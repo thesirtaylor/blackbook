b00bb874f57ace537b0d1eb0e46339bd2a86bb56
thesirtaylor <ajayiolatunji17@gmail.com>
2020-09-07 13:58:55 +0100

    little change

--- router/routes.js
+++ router/routes.js
@@ -11,8 +11,7 @@ const verify = require('../lib/jwt').CHECK_TOKEN;
 
 module.exports = function (app){
     router.get('/', function (req, res) {
-        res.send(`BlackStoryBook Project \n Welcome to the
-         Development Server \n Say a Prayer for us.`)
+        res.send("BlackStoryBook Project \n Welcome to the Development Server \n Say a Prayer for us.")
       });
     router.post('/api/user/create', userSignup.signup);
     router.post('/api/user/verify', userVerifymail.verify);
