f30bfad7b85998c6f9ceba5ad015d5ebb8b4723b
thesirtaylor <ajayiolatunji17@gmail.com>
2021-01-06 23:17:01 +0100

    redis still problem

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ let express = require("express"),
   mongoose = require("mongoose"),
   logger = require("./lib/logger"),
   app = express(),
+  kue = require("kue"),
+  url = require("url"),
+  redis = require("kue/node_modules/redis"),
   configure = require("./server/configure");
 
 function logtime() {
@@ -15,8 +18,19 @@ function logtime() {
 }
 //------------------------------------------------------------------SET APP MIDDLEWARE PARAMETER-----------------------------------------------//
 //------------------------------------------------------------------                            -----------------------------------------------//
+
+kue.redis.createClient = function () {
+  var redisUrl = url.parse(process.env.REDISTOGO_URL),
+    client = redis.createClient(redisUrl.port, redisUrl.hostname);
+  if (redisUrl.auth) {
+    client.auth(redisUrl.auth.split(":")[1]);
+  }
+  return client;
+};
+
 app.set("view", __dirname + "/view"); //google how to set view path
 app.set("port", process.env.PORT || 1010); //google how to set dynamic port
+app.use(kue.app);
 app = configure(app);
 
 //-----------------------------------------------------------------CONNECT to MONGOOSE---------------------------------------------------------//
--- controller/payment.js
+++ controller/payment.js
@@ -9,10 +9,26 @@ let User = require("../model/users").user,
   ERR = require("../util/error"),
   SUCCESS = require("../util/success"),
   HTTP_STATUS = require("../util/httpstatus"),
-  redis = require("../lib/redis").redisClient,
-  kue = require("kue"),
-  queue = kue.createQueue(redis);
+  // redis = require("../lib/redis").redisClient,
+  kue = require("kue");
 const split_value = 0.25;
+
+var url = require("url"),
+  redis = require("kue/node_modules/redis");
+
+// make sure we use the Heroku Redis To Go URL
+// (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
+
+kue.redis.createClient = function () {
+  var redisUrl = url.parse(process.env.REDISTOGO_URL),
+    client = redis.createClient(redisUrl.port, redisUrl.hostname);
+  if (redisUrl.auth) {
+    client.auth(redisUrl.auth.split(":")[1]);
+  }
+  return client;
+};
+
+var queue = kue.createQueue();
 
 async function saveWebhook(request, job, done) {
   try {
